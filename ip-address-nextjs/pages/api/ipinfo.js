247d63c8170ae3c693b48e3f335d1cb621025a7e
John Higginson <jhigginson@gmail.com>
2022-06-21 13:45:21 -0700

    cleanup api

--- ip-address-nextjs/pages/api/ipinfo.js
+++ ip-address-nextjs/pages/api/ipinfo.js
@@ -10,52 +10,6 @@ const isIP = (input) => {
   return /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(input);
 }
 
-
-// const fillInfo = (data) => {
-//   if (data.ip) {
-//     // console.log(data);
-//     const locInfo = {
-//       ip: data.ip,
-//       location: `${data.location.city}, ${data.location.region} ${data.location.postalCode}`,
-//       timezone: `UTC ${data.location.timezone}`,
-//       isp: data.isp,
-//       lat: data.location.lat,
-//       long: data.location.lng
-//     };
-//     props.onSubmit(locInfo);
-//   } else {
-//     alert("Hmm... something went wrong with calling the API.");
-//   }
-// };
-
-// const callIpify = (input) => {
-//   console.log("ipify called");
-//   const api_key = process.env.NEXT_PUBLIC_IPIFY_KEY;
-//   const api_url = 'https://geo.ipify.org/api/v2/country,city?';
-//   let requestField = "";
-//   if (isIP(input)) {
-//     requestField = '&ipAddress=';
-//   } else if (isDomain(input)) {
-//     requestField = '&domain=';
-//   } else if (input === "") {
-//     requestField = "";
-//   }
-//   else {
-//     alert("Please enter a valid ip or domain name");
-//     return false;
-//   }
-
-//   const url = `${api_url}apiKey=${api_key}${requestField}${input}`;
-//   console.log(url);
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(doAfter)
-//     .catch((e) => {
-//       console.error(e);
-//       return false;
-//     });
-// }
-
 function handler(req, res) {
   if (req.method === 'GET') { //do this for each verb you want to support in your api
     const ip = req.query.ipAddress ?? null;
@@ -75,10 +29,8 @@ function handler(req, res) {
     const api_url = process.env.IPIFY_URL;
 
     const url = `${api_url}apiKey=${api_key}${requestField}${param}`;
-    console.log(url);
 
-    let rStatus = 404;
-    let rJson = {};
+
 
     fetch(url)
       .then((ipifyRes) => ipifyRes.json())
@@ -98,7 +50,7 @@ function handler(req, res) {
       })
       .catch((error) => {
         return res.status(404).json({ message: "couldn't fetch data from ipify" });
-      }).finally(() => res.status(404).json({ message: "this shouldn't happen" }));
+      });
   }
 };
 
