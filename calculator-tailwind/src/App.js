05133f6253894f3e9fe51778a20cb29a330c540d
John Higginson <jhigginson@gmail.com>
2022-06-24 18:32:48 -0700

    initial commit

---
+++ calculator-tailwind/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Calculator</h1>
+      </header>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
